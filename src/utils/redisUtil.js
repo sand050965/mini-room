bcaf5bd8e76168557db5e94238fb5a7769dd8cae
sand050965 <sand050965@gmail.com>
2023-03-16 00:07:56 +0800

    fix: Fixed redis connect parameters

--- src/utils/redisUtil.js
+++ src/utils/redisUtil.js
@@ -1,9 +1,9 @@
 const redis = require("redis");
 
-const client = redis.createClient({
-	url: process.env.AWS_ELASTICACHE_REDIS_URL,
-	tls: {},
-});
+const client = redis.createClient(
+	process.env.AWS_ELASTICACHE_REDIS_PORT,
+	process.env.AWS_ELASTICACHE_REDIS_URL
+);
 
 client.on("connect", () => {
 	console.log("Connected to Redis");
